f7dd495563612b74db0767d4817728f871d4bf92
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-26 20:11:12 +0900

    Add settings
    eslint, babel 설정등을 추가합니다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-              test:/\.js$/,
-              exclude: /node_modules/,
-            },
-        ],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
     },
+  },
 };
